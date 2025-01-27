be0c37de796300c70eb1fc7f4ebe744fd0c5254d
Dorado <dorado.gumoi24@triana.salesianos.edu>
2025-01-27 14:29:57 +0100

    asd

---
+++ perfilespostgresql/src/main/java/com/salesianos/perfilespostgresql/PerfilespostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.perfilespostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfilespostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfilespostgresqlApplication.class, args);
+	}
+
+}
